547056322fc70a2243ad70de2bb24e3acd09e7c6
violeta-kuznecova <violetakuznevowa8@gmail.com>
2025-04-28 13:25:50 +0300

    Add files via upload

---
+++ skolens.py
@@ -0,0 +1,27 @@
+#2.uzdevums
+from skolens import Skolens
+class Skola:
+    def __init__(self):
+        self.skoleni = []
+        def pievienot_skolenu(self, vards, uzvards, sekmes):
+        self.skoleni.append(jaunais_skolens)
+        def paradit_visus_skolenus(self):
+            for skolens in self.skoleni:
+                print(skolens)
+        
+        #3.uzdevums
+        from skolens import Skolens
+        class Skola:
+            def__init__(self,nosaukums):
+                self.nosaukums = nosaukums
+                self.skoleni = []
+                def pievienot_skolenu(self, skolens):
+                    self.skoleni.append(skolens)
+                    def paradit_visus_skolenus(self):
+                        for skolens in self.skoleni:
+                            print(skolens)
+                            def labakais(self):
+                                if not self.skoleni:
+                                    return None
+                                labakais_skolens = max(self.skoleni, key=lambda skolens: skolens.videja_atzime())
+                                return labakais_skolens
